6dbba0437821a2225cc00aee865fac478fe13361
JimKrow <krowstudio@icloud.com>
2022-03-23 17:41:16 +0100

    commentaires et déclaration de fonction

--- functions.js
+++ functions.js
@@ -1,18 +1,25 @@
+
+// Fonction qui trouve toutes les occurences possible d'un chiffrement césar d'une expression passée en paramètre
 export function forceCaesar(str) {
     let res = "";
     let alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    for (let i = 0;i < 25;i++) {
-        for(let j = 0; j < str.length;j++) {
+    for (let i = 0; i < 25; i++) {
+        for (let j = 0; j < str.length; j++) {
             let currentChar = str[j]
-            if(currentChar == " "){
+            if (currentChar == " ") {
                 break;
             }
             let position = alphabet.indexOf(currentChar)
-            let newCHar = alphabet[(position+i)%25]
+            let newCHar = alphabet[(position + i) % 25]
             res += newCHar;
         }
         console.log(res)
         res = "";
     }
+}
+
+// Fonction de calcul du volume d'un cône
+export function coneVolume() {
+
 }
